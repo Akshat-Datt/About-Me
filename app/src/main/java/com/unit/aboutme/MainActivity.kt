81c060514500cc2ae7bf18f77448cba7717b3c47
Akshat-Datt <dattakshat31@gmail.com>
2025-12-28 12:23:57 +0530

    inflating main activity with activity_main layout

--- app/src/main/java/com/unit/aboutme/MainActivity.kt
+++ app/src/main/java/com/unit/aboutme/MainActivity.kt
@@ -16,32 +16,6 @@ import com.unit.aboutme.ui.theme.AboutMeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            AboutMeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AboutMeTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
