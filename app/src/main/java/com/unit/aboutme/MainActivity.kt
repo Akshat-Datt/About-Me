6cfdfc7114061c9f4d646c6ab60f96f1f258491a
Akshat-Datt <dattakshat31@gmail.com>
2025-12-30 22:54:49 +0530

    toggling visibility of views on getting input of nickname and hiding edit text and button accordingly

--- app/src/main/java/com/unit/aboutme/MainActivity.kt
+++ app/src/main/java/com/unit/aboutme/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.unit.aboutme
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,5 +23,24 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnNickName : Button = findViewById<Button>(R.id.btn_submitNickName)
+
+        btnNickName.setOnClickListener {
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View){
+        val nickNameInput = findViewById<EditText>(R.id.et_nickName)
+        val nickNameOutput = findViewById<TextView>(R.id.tv_nickName)
+
+        nickNameOutput.text = nickNameInput.text
+        nickNameInput.visibility = View.GONE
+        view.visibility = View.GONE
+        nickNameOutput.visibility = View.VISIBLE
+
+        val keyboardTouchPad = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        keyboardTouchPad.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
